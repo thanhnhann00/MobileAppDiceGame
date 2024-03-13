561e66c56a32cfdc067766bcde44ce5d10efec3e
ajc7311 <ajc7311@gmail.com>
2024-03-13 13:45:55 -0400

    Added 4-20 sided dice files
    Added the images for each of the various dice types and began on diceSelect.
    D4, 8, 10, 12, & 20
    Also merge conflict resolution

--- app/src/main/java/com/example/dicerollapp/DiceSelectFragment.java
+++ app/src/main/java/com/example/dicerollapp/DiceSelectFragment.java
@@ -4,11 +4,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.fragment.app.Fragment;
 
 public class DiceSelectFragment extends Fragment {
 
+    private final int[] diceOptions = new int[]{R.drawable.dice4, R.drawable.d6, R.drawable.dice8, R.drawable.dice10, R.drawable.dice12, R.drawable.dice20};
+    // Define a Random object
+    private ImageView dice4;
+    private ImageView d6;
+    private ImageView dice8;
+    private ImageView dice10;
+    private ImageView dice12;
+    private ImageView dice20;
     public DiceSelectFragment() {
     }
 
