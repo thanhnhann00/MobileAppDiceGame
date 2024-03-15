49f4636e07ee079bbe6667461a89acf653f74d68
Alexis Martinez <martinez.656@buckeyemail.osu.edu>
2024-03-15 12:18:00 -0400

    Added the accelerometer sensor so that shaking the phone also rolls the dice

--- app/src/main/java/com/example/dicerollapp/DiceActivity.java
+++ app/src/main/java/com/example/dicerollapp/DiceActivity.java
@@ -1,17 +1,13 @@
 package com.example.dicerollapp;
 
 import android.content.Intent;
-import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -26,12 +22,9 @@ import com.example.dicerollapp.navigation_drawer.DataModel;
 import com.example.dicerollapp.navigation_drawer.DrawerItemCustomAdapter;
 import java.util.Objects;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.HashMap;
-import java.util.Objects;
-import java.util.Random;
 
 public class DiceActivity extends AppCompatActivity {
 
